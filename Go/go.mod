a3339ac140d9f98f504b0e3aa4f664e732eedbfc
Elizabeth Conwell <esophiac@gmail.com>
2025-05-20 19:27:12 -0400

    bootstrap in main

---
+++ Go/go.mod
@@ -0,0 +1,5 @@
+module Go
+
+go 1.23.5
+
+require github.com/seehuhn/mt19937 v1.0.0
--- Go/main.go
+++ Go/main.go
@@ -2,8 +2,66 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+
+	"github.com/seehuhn/mt19937"
 )
 
+// create a slice of random floats that is lngth long
+func randSlice(lngth int, max int) []float64 {
+
+	var result []float64
+
+	for i := 0; i < lngth; i++ {
+		result = append(result, float64(rand.Intn(max))) // Generates random integers between 0 and max
+	}
+
+	return result
+}
+
+// given a slice of ints, create a new slice that is a sample with replacement
+// the same size as the initial slice
+func sampledSlice(inital []float64) (final []float64) {
+
+	sampleRange := len(inital)
+
+	for i := 0; i < sampleRange; i++ {
+		final = append(final, inital[rand.Intn(sampleRange)])
+	}
+
+	return final
+}
+
+// Resample the data with replacement n times
+func boots(floatSlice []float64, n int) (bootsResult [][]float64) {
+
+	for i := 0; i < n; i++ {
+
+		// create a new slice with replacement
+		sample := sampledSlice(floatSlice)
+
+		// add it to the result
+		bootsResult = append(bootsResult, sample)
+
+	}
+
+	return bootsResult
+
+}
+
+//Compute desired statistic n times to generate a distribution of estimated statistics
+
+//Determine standard error/confidence interval for the bootstrapped statistic from the bootstrapped distribution
+
 func main() {
-	fmt.Println("hello")
+
+	// create a random generator to use in all the functions
+	rand.New(mt19937.New())
+
+	newSlice := randSlice(5, 10)
+	fmt.Println(newSlice)
+
+	newSample := boots(newSlice, 5)
+	fmt.Println(newSample)
+
 }
